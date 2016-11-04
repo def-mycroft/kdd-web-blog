d8ed627cf0f0320a62b18b51d08bd9b1c64fbc0c
def-mycroft23 <dasenbrj@ksu.edu>
2016-11-04 10:02:06 -0500

    before clearing tests out of db_funcs

--- model/db_funcs.py
+++ model/db_funcs.py
@@ -1,4 +1,32 @@
 import db_connection
 
+conn = db_connection.database_connection()
+cur = conn.cursor()
+
+cur.execute('select * from post')
+data = cur.fetchall()
+
+print('here is the data')
+print(data)
+
+
+def rows_to_dicts(rows):
+    # [Row, Row, Row]
+    # row.keys()
+
+    dicts = []
+    for row in rows:
+
+        dicts.append(
+            dict(map(
+                lambda key, value: (key, value),
+                row.keys(), row
+            )
+        ))
+
+    return dicts
+
+print(rows_to_dicts(data))
+
 
 
