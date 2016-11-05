89061e7c8ffb582727297d6e449e14aa89e0bafb
def-mycroft23 <dasenbrj@ksu.edu>
2016-11-05 18:54:13 -0500

    have functional tool for creating new post

--- model/db_funcs.py
+++ model/db_funcs.py
@@ -1,4 +1,36 @@
+from flask import request
 from model import db_connection
+import markdown
+import time
+
+
+def commit_new_post():
+    """Commits a new post to the database"""
+
+    conn = db_connection.database_connection()
+    cur = conn.cursor()
+
+    title = request.form['post-title']
+    content = request.form['post-content']
+
+    cur.execute(
+        """
+        INSERT INTO post (title, content, date_created, author_id)
+        VALUES(?,?,?,?)
+        """, (title, content, int(time.time()), 1)
+        # TODO I have the author id set to 1 for testing, will need
+        # to pull the actual author id from the session later.
+    )
+
+    conn.commit()
+
+    post_id = cur.lastrowid
+
+    # TODO I might want to create rudimentary error handline here
+
+    return post_id
+
+
 
 def fetch_a_specific_post(post_id):
     """Fetches a particular post given a post_id"""
@@ -8,13 +40,16 @@ def fetch_a_specific_post(post_id):
 
     cur.execute(
         """
-        SELECT * FROM post WHERE id=%s
-        """ % post_id
+        SELECT * FROM post WHERE id=?
+        """, (post_id,)
     )
 
-    data = cur.fetchall()
+    data = rows_to_dicts(cur.fetchall())[0]
 
-    return rows_to_dicts(data)[0]
+    data['content'] = markdown.markdown(data['content'])
+    # TODO write function to convert dates and use it here.
+
+    return data
 
 
 def fetch_index_posts():
@@ -32,9 +67,13 @@ def fetch_index_posts():
         """
     )
 
-    data = cur.fetchall()
+    data = rows_to_dicts(cur.fetchall())
 
-    return rows_to_dicts(data)
+    for post in data:
+        post['content'] = markdown.markdown(post['content'])
+        # TODO write function to convert dates and use it here.
+
+    return data
 
 
 def rows_to_dicts(rows):
--- post.py
+++ post.py
@@ -1,25 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect
 from model import db_funcs, db_connection
 
 post = Blueprint('post', __name__)
-
-
-@post.route('/<int:post_id>', methods=['get'], strict_slashes=False)
-def show_or_update_post(post_id):
-    """Shows an individual post"""
-    post = db_funcs.fetch_a_specific_post(post_id)
-    print(post)
-    return render_template('post.html', 
-                           post_title=post['title'], 
-                           post_author_id=post['author_id'], 
-                           post_date_created=post['date_created'], 
-                           post_content=post['content'])
-
-
-@post.route('/new', methods=['GET'], strict_slashes=False)
-def new_post():
-    """Renders form for new post"""
-    return render_template('new_or_update_post.html')
 
 
 @post.route('/', methods=['GET'], strict_slashes=False)
@@ -29,19 +11,40 @@ def show_index():
     return render_template('index.html', posts=posts)
 
 
+@post.route('/<int:post_id>', methods=['get'], strict_slashes=False)
+def show_individual_post(post_id):
+    """Shows an individual post"""
+    post = db_funcs.fetch_a_specific_post(post_id)
+
+    return render_template(
+        'post.html',
+        post_title=post['title'],
+        post_author_id=post['author_id'],
+        post_date_created=post['date_created'],
+        post_content=post['content']
+    )
+
+
+@post.route('/new', methods=['GET'], strict_slashes=False)
+def new_post():
+    """Renders form for new post"""
+    return render_template('new_or_update_post.html')
+
+
+@post.route('/commit-new', methods=['POST'], strict_slashes=False)
+def commit_post():
+    """Commits a new post to the database"""
+    # TODO it would be nice to direct toward the newly-created post.
+    post_id = db_funcs.commit_new_post()
+    return redirect('post/%s' % post_id)
+
+
 @post.route('/<int:post_id>/edit', methods=['GET'], strict_slashes=False)
 def edit_post(post_id):
     """Returns the edit form with post content"""
     # TODO write function that pulls data from the database and inserts into edit form.
     # Will need to get data from the database and insert into htmlform 
     return render_template('new_or_update_post.html')
-
-
-@post.route('/', methods=['POST'], strict_slashes=False)
-def commit_post(post_id):
-    """Commits a new post to the database"""
-    # TODO Write a db_Funcs method thae takes post data and commits to database
-    return redirect('/<int:post_id>')
 
 
 # I just have this disabled for now because it is throwing error, probably because of ussie with post_id
